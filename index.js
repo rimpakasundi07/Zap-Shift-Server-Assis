554bf6a9b0a3d7a652319560aec5607837f2c6f1
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-02 23:15:36 +0530

    Add search functionality to GET /users endpoint with regex and limit

--- index.js
+++ index.js
@@ -91,7 +91,15 @@ async function run() {
 
     // users related APIS
     app.get("/users", verifyFBToken, async (req, res) => {
-      const cursor = userCollection.find();
+      const searchText = req.query.searchText;
+      const query = {};
+      if (searchText) {
+        query.displayName = { $regex: searchText, $options: "i" };
+      }
+      const cursor = userCollection
+        .find(query)
+        .sort({ createdAt: -1 })
+        .limit(5);
       const result = await cursor.toArray();
       res.send(result);
     });
