32a96b4333b988ab54a68b47f4ef2708f1f13bfe
rimpa kasundi <rimpakasundi@gmail.com>
2026-01-07 19:40:35 +0530

    Add GET /parcels/rider endpoint for filtering parcels by rider email and delivery status

--- index.js
+++ index.js
@@ -175,6 +175,27 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/parcels/rider", async (req, res) => {
+      const { riderEmail, deliveryStatus } = req.query;
+      const query = {};
+      if (riderEmail) {
+        query.riderEmail = riderEmail;
+      }
+      if (deliveryStatus) {
+        query.deliveryStatus = deliveryStatus;
+      }
+      const cursor = parcelsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.findOne(query);
+      res.send(result);
+    });
+
     // to add
 
     app.post("/parcels", async (req, res) => {
@@ -358,13 +379,6 @@ async function run() {
       res.send({ url: session.url });
     });
     // payment
-
-    app.get("/parcels/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await parcelsCollection.findOne(query);
-      res.send(result);
-    });
 
     // payment success related
 
